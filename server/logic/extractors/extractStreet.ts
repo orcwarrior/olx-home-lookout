dc34a4b034841cedc7bf619ab94cfde691a7d073
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-17 01:34:25 +0200

    server: extractStreet improvs

--- server/logic/extractors/extractStreet.ts
+++ server/logic/extractors/extractStreet.ts
@@ -4,17 +4,17 @@ const DIGIT_RGX = /\d/;
 
     // const STREET_RGX = /((ulic\w*)|(ul)|(adres)|(zlokalizowa\w+ na))\W{1,2}(((\w+(\.\w+)?\s+){1,3}\d+[a-f,r]?)|(\w+))(\s|[^mzp]|,|r)/gi;
 function extractStreet(title, description) {
-    const STREET_RGX = /((ulic\w*)|(ul)|(adres)|(zlokalizowa\w+ na))\W{1,2}(((\w+(\.\w+)?(\s|\-)+){1,3}\d+[a-f,r]?)|(\w+(\s|\-)*){1,2})(\s|[^mzp]|,|r)/gi;
+    const STREET_RGX = /((ulic\w*)|(ul)|(adres)|(zlokalizowa\w+ na)|(okolice))\W{1,2}(((([\.\w]+)?(\s|\.|\-)+){1,3}[\d/]+[a-f,r]?)|(\w+[\.\- ]*){1,3})(\s|[^mzp]|,|\.|r)/gi;
     const fullStr = deburr(`${title} ${description}`);
     let streetMatch, groups;
     while (groups = STREET_RGX.exec(fullStr)) {
-        const streetGr = groups[6];
+        const streetGr = groups[7];
         if (
             (!streetMatch && streetGr && streetGr.length > 2)
             || streetMatch && (!DIGIT_RGX.test(streetMatch) && DIGIT_RGX.test(streetGr))
     )
         {
-            streetMatch = streetGr;
+            streetMatch = streetGr.replace(/-(\w)/, "- $1");
         }
     }
 
