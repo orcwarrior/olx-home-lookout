5a595134d3aa56e2b4d65934459d5f4879465e28
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-17 03:34:21 +0200

    server: show db uri

--- server/db/init.ts
+++ server/db/init.ts
@@ -5,10 +5,11 @@ import seedDatabase from "./seeds/init";
 
 
 (async () => {
-    const {dropDb, seedDb} = db;
+    const {dropDb, seedDb, uri} = db;
     console.log("DB Config: ", {dropDb, seedDb});
     const dbExisted = !(await createDbIfNotExists());
 
+    console.log(`Connecting to db: ${uri}`);
     if (db.initDbOnce && dbExisted) {
         console.log("SERVER__DB_INIT_ONCE is true, and DB was already inited, skipping init...");
         return process.exit();
