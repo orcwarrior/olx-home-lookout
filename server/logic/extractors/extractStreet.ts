0e65b42a20202c7fa74c835254def9d15db68f41
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-19 04:20:06 +0200

    server/logic: extractStreet regex update

--- server/logic/extractors/extractStreet.ts
+++ server/logic/extractors/extractStreet.ts
@@ -5,7 +5,7 @@ const DIGIT_RGX = /\d/;
     // const STREET_RGX = /((ulic\w*)|(ul)|(adres)|(zlokalizowa\w+ na))\W{1,2}(((\w+(\.\w+)?\s+){1,3}\d+[a-f,r]?)|(\w+))(\s|[^mzp]|,|r)/gi;
     // const STREET_RGX = /((ulic\w*)|(ul)|(adres)|(zlokalizowa\w+ na)|(okolice))\W{1,2}(((([\.\w]+)?(\s|\.|\-)+){1,3}[\d/]+[a-f,r]?)|(\w+[\.\- ]*){1,3})(\s|[^mzp]|,|\.|r)/gi;
 function extractStreet(title, description) {
-    const STREET_RGX = /((ulic\w*)|(przy (ul)?)|(ul)|(adres)|(zlokalizowa\w+ na (ul))|(okolice))[\.\W]{1,2}(((((\w{1,2}\.)?\w+)?(\s|\.|\-)+){1,3}[\d/]+[a-f,r]?)|(((\w\.)|(\w+))[\- ]{0,3}){1,3})(\s|[^mzp]|,|\.|r)/gi;
+    const STREET_RGX = /((ulic\w*[^\.])|(przy (ul)?)|(ul)|(adres)|(zlokalizowa\w+ na (ul))|(okolice))[\.\W]{1,2}(((((\w{1,2}\.)?\w+)?([ .\/\-])+){1,3}[\d/]+[a-f,r]?)|(((\w\.)|(\w+))[\- \/]{0,3}){1,3})(\s|[^mzp]|,|\.|r)/gi;
     const fullStr = deburr(`${title} ${description}`);
     let streetMatch, groups;
     while (groups = STREET_RGX.exec(fullStr)) {
