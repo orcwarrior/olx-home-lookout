9b5cbbb6b204068dc9655664b761afc67c12b633
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-23 05:00:33 +0200

    server/db: recalculateLookoutAvgRank fixed

--- server/db/logic/recalculateLookoutAvgRank.ts
+++ server/db/logic/recalculateLookoutAvgRank.ts
@@ -9,22 +9,28 @@ async function recalculateLookoutAvgRank(lookoutId): Promise<LookoutRequest[]> {
         .where({lookoutRequestId: lookoutId})
         .andWhereNot({userReviewStatus: USER_REVIEW_STATUS.REJECTED});
 
+
+
     const offersCnt = Number(offersCntDb);
-    const rejectedPart = 0.02; // 800 offers -> 16 lowest & 16 highest
+    const rejectedPart = 0.03; // 800 offers -> 16 lowest & 16 highest
     const offset = Math.round(offersCnt * rejectedPart);
     const limit = Math.round(offersCnt - offset * 2);
     console.log({offersCnt, offset, limit, offersCntDb})
+
+    const normalizedAvgRankSQL = `
+    (select avg(normalized_offers.rank)
+    FROM (
+        SELECT "rank" FROM "Offers" 
+        WHERE ("lookoutRequestId" = ${lookoutId} AND NOT "userReviewStatus" = '${USER_REVIEW_STATUS.REJECTED}') 
+        ORDER BY "rank" ASC 
+        LIMIT ${limit} offset ${offset}) as normalized_offers)
+`
+
     return knex<LookoutRequest>("LookoutRequests")
         .where({id: lookoutId})
         .update<LookoutRequest>({
             // @ts-ignore
-            avgRank: knex<Offer>("Offers")
-                .avg("rank")
-                .where({lookoutRequestId: lookoutId})
-                .andWhereNot({userReviewStatus: USER_REVIEW_STATUS.REJECTED})
-                .orderBy("rank")
-                .offset(offset)
-                .limit(limit)
+            avgRank: knex.raw(normalizedAvgRankSQL)
         })
         .returning("*");
 }
