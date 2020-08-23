df515f01e39949adf3c895eac0adbaa56df5e84e
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-23 15:06:49 +0200

    client: fixed grid mobile responsivness

--- client/pages/lookout/[uuid].js
+++ client/pages/lookout/[uuid].js
@@ -2,27 +2,34 @@ import React, { useContext } from "react";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/react-hooks";
 import LOOKOUT_OFFERS_QUERY from "../../gql-queries/lookoutOffers.graphql";
-import { Grid, Box, InfiniteScroll } from "grommet";
+import { Grid, Box, InfiniteScroll, ResponsiveContext } from "grommet";
 import { OfferCard } from "@components/Offer";
 import { ReportHeader } from "@components/Report/ReportHeader";
 import { Layout } from "@components/common/Layout";
 import { NotFound } from "@components/common/NotFound";
 import { withPage } from "../../next-utils";
 
-const ReportPage = withPage(() => {
+const LookoutPage = withPage(() => {
   const router = useRouter();
+  const size = useContext(ResponsiveContext);
+  console.log(`size: `, size);
   const {uuid} = router.query;
   const defaultWhere = {LookoutRequest: {hash: {_eq: uuid}}, userReviewStatus: {_neq: "REJECTED"}};
   const {data, refetch: refetchOffers} = useQuery(LOOKOUT_OFFERS_QUERY,
-      {variables: {where: defaultWhere}})
+      {variables: {where: defaultWhere, orderBy: {rank: "desc"}}})
   const offers = (data?.Offers_connection?.edges || [])
       .map(({node}) => node)
+
+  const gridColumns = (size === "small")
+  ? {"size": "flex", "count": "fit"}
+  : {"size": "500px", "count": "fit"}
+  const wrapperOverflow = (size === "small") ? "visible" : "auto";
 
 
   return <Layout>
     <ReportHeader refetchOffers={refetchOffers}/>
-    <Box overflow="auto" gap="none" fill="vertical" pad="small">
-      {offers.length ? <Grid gap="small" columns={{"size": "500px", "count": "fit"}} fill="horizontal" rows={[""]}>
+    <Box overflow={wrapperOverflow} gap="none" fill="vertical" pad="small">
+      {offers.length ? <Grid gap="small" columns={gridColumns} fill="horizontal" rows={[""]}>
         <InfiniteScroll items={offers} step={12}>
           {(offer) => <OfferCard {...offer} key={offer.id}/>}
         </InfiniteScroll>
@@ -31,4 +38,4 @@ const ReportPage = withPage(() => {
   </Layout>
 })
 
-export default ReportPage;
+export default LookoutPage;
