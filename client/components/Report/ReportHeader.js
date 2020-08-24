28134fea41956988bb15e3a93283f9d40bfb506b
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-24 04:37:50 +0200

    client: ReportHeader dirty implementation of collapsing header

--- client/components/Report/ReportHeader.js
+++ client/components/Report/ReportHeader.js
@@ -5,6 +5,8 @@ import { Achievement, Ascend, Currency, Descend, History, Lounge } from "grommet
 import { debounce } from "@utils/debounce";
 import "./ReportHeader.scss";
 import useQueryString from "@hooks/useQueryString";
+import { useRecoilState } from "recoil";
+import { gridScrollState } from "../../pages/lookout/[uuid]";
 
 const PRICE_RANGE = [0, 3000];
 const AREA_RANGE = [10, 120];
@@ -69,7 +71,7 @@ const QSHookJSONOpts = {
       try {
         return JSON.parse(str);
       } catch (err) {
-        console.log(`err: `, err);
+        // console.log(`err: `, err);
         return undefined;
       }
     }
@@ -79,7 +81,7 @@ const QSHookJSONOpts = {
       try {
         return JSON.stringify(v);
       } catch (err) {
-        console.log(`err: `, err);
+        // console.log(`err: `, err);
         return undefined;
       }
     }
@@ -94,8 +96,12 @@ const ReportHeader = ({refetchOffers}) => {
   const [orderBy, setOrderBy] = useQueryString("order", {rank: "desc"}, QSHookJSONOpts)
   const [queryVarsJSON, setQueryVarsJSON] = useState(JSON.stringify(queryDefaults))
 
+  const [gridScroll, setGridScroll] = useRecoilState(gridScrollState);
+
   const size = useContext(ResponsiveContext);
   const isMobile = size === "small";
+  const isCollapsed = isMobile && !!gridScroll;
+  console.log(`gridScroll: `, gridScroll);
 
   const refetchDebounced = React.useCallback(debounce(refetchOffers, 1500), [])
 
@@ -111,111 +117,114 @@ const ReportHeader = ({refetchOffers}) => {
     fill: "horizontal",
     pad: {horizontal: "xlarge"}
 
-  } : {fill: "vertical"};
+  } : {fill: "vertical", width: "auto"};
   const sortByItemProps = {setOrderBy, orderBy};
 
+  const headerClass = (isCollapsed) ? "lookout-header collapsed" : "lookout-header";
   return (
-      <Box align="center" justify="center" fill="horizontal" direction="row-responsive" basis="xsmall" flex="grow"
-           pad={{"horizontal": "large"}} background="black">
+      <Box align="center" justify="center" fill="horizontal" direction="row-responsive" basis="xsmall"
+           pad={{"horizontal": "large"}} background="black" height="medium" className={headerClass}>
         <Box align="center" justify="center" flex="shrink" {...logoProps}>
           <Image src="/assets/home-lookout-logo.png" fit="contain" fill={logoProps.fill}/>
         </Box>
 
-        <Box align="stretch" justify="center" flex="grow" direction="row-responsive" gap="medium">
-          <Box align="center" justify="between" gap="small" pad="medium" basis="1/4">
-            <Box justify="center" align="center" direction="row" gap="small">
-              <Text>cena</Text> <Text size="small">{pricesRange[0]} - {pricesRange[1]}PLN</Text>
-            </Box>
-            <RangeSelector
-                direction="horizontal"
-                invert={false}
-                min={PRICE_RANGE[0]}
-                max={PRICE_RANGE[1]}
-                size="small"
-                round="small"
-                values={pricesRange}
-                onChange={setPricesRange}
-            />
-          </Box>
-          <Box align="center" justify="between" gap="small" pad="medium" basis="1/4">
-            <Box justify="center" align="center" direction="row" gap="small">
-              <Text>powierzchnia</Text> <Text size="small">{areaRange[0]} - {areaRange[1]}m²</Text>
-            </Box>
-            <RangeSelector
-                direction="horizontal"
-                invert={false}
-                min={AREA_RANGE[0]}
-                max={AREA_RANGE[1]}
-                size="small"
-                round="small"
-                values={areaRange}
-                onChange={setAreaRange}
-            />
-          </Box>
-          <Box align="center" justify="between" gap="small" pad="medium" basis="1/4">
-            <Box justify="center" align="center" direction="row" gap="small">
-              <Text>komfort</Text> <Text size="small">{comfortRange[0]} - {comfortRange[1]}</Text>
-            </Box>
-            <RangeSelector
-                direction="horizontal"
-                invert={false}
-                min={COMFORT_RANGE[0]}
-                max={COMFORT_RANGE[1]}
-                size="small"
-                round="small"
-                values={comfortRange}
-                onChange={setComfortRange}
-            />
-          </Box>
-          <Box align="center" justify="between" gap="medium" pad="small" basis="1/4"
-               style={{marginTop: "14px"}}>
-            <MultiToggle
-                options={USER_REVIEW_FILTER}
-                selectedOption={userReviewStatus}
-                onSelectOption={setUserReviewStatus}
-                label="Show offers"
-            />
-          </Box>
-        </Box>
-
-        <Box align="center" justify="center" direction="column">
-          <DropButton label="Sortowanie" dropAlign={{"top": "bottom"}} dropContent={(
-              <Box align="center" justify="center" pad="medium" name="dropContent" background={"black"}
-                   animation="fadeIn" hoverIndicator={false} border={{"size": "small"}} gap="small">
-                <SortItem icon={Achievement} sortObj={{rank: "desc"}} {...sortByItemProps}>
-                  Ocena
-                </SortItem>
-                <SortItem icon={History} sortObj={{createdAt: "desc"}} {...sortByItemProps}>
-                  Od najnowszych
-                </SortItem>
-                <SortItem icon={Lounge} sortObj={{indicators_comfort: "desc"}} {...sortByItemProps}>
-                  Komfort
-                </SortItem>
-                <SortItem icon={Lounge} sortObj={{deviationAvgM2Price: "asc"}} {...sortByItemProps}>
-                  Odchyl. cenowe 📈
-                </SortItem>
-                <SortItem icon={Currency} sortObj={{indicators_deal: "desc"}} {...sortByItemProps}>
-                  Okazja cenowa
-                </SortItem>
-                <SortItem sortObj={{prices_perM2: "asc"}} {...sortByItemProps}>
-                  Cena m²
-                </SortItem>
-                <SortItem icon={Ascend} sortObj={{prices_full: "asc"}} {...sortByItemProps}>
-                  Cena {/* rosnaco */}
-                </SortItem>
-                <SortItem icon={Descend} sortObj={{prices_full: "desc"}} {...sortByItemProps}>
-                  Cena {/* malejaco */}
-                </SortItem>
-                <SortItem icon={Ascend} sortObj={{attrs_area: "asc"}} {...sortByItemProps}>
-                  Powierzchnia {/* rosnaco */}
-                </SortItem>
-                <SortItem icon={Descend} sortObj={{attrs_area: "desc"}} {...sortByItemProps}>
-                  Powierzchnia {/* malejaco */}
-                </SortItem>
-
+        <Box className="menu-collapsible">
+          <Box align="stretch" justify="center" flex="grow" direction="row-responsive" gap="medium">
+            <Box align="center" justify="between" gap="small" pad="medium" basis="1/4">
+              <Box justify="center" align="center" direction="row" gap="small">
+                <Text>cena</Text> <Text size="small">{pricesRange[0]} - {pricesRange[1]}PLN</Text>
               </Box>
-          )}
-                      plain={false} reverse={false} dropProps={{"elevation": "xsmall", "plain": true}}/>
+              <RangeSelector
+                  direction="horizontal"
+                  invert={false}
+                  min={PRICE_RANGE[0]}
+                  max={PRICE_RANGE[1]}
+                  size="small"
+                  round="small"
+                  values={pricesRange}
+                  onChange={setPricesRange}
+              />
+            </Box>
+            <Box align="center" justify="between" gap="small" pad="small" basis="1/4">
+              <Box justify="center" align="center" direction="row" gap="small">
+                <Text>powierzchnia</Text> <Text size="small">{areaRange[0]} - {areaRange[1]}m²</Text>
+              </Box>
+              <RangeSelector
+                  direction="horizontal"
+                  invert={false}
+                  min={AREA_RANGE[0]}
+                  max={AREA_RANGE[1]}
+                  size="small"
+                  round="small"
+                  values={areaRange}
+                  onChange={setAreaRange}
+              />
+            </Box>
+            <Box align="center" justify="between" gap="small" pad="medium" basis="1/4">
+              <Box justify="center" align="center" direction="row" gap="small">
+                <Text>komfort</Text> <Text size="small">{comfortRange[0]} - {comfortRange[1]}</Text>
+              </Box>
+              <RangeSelector
+                  direction="horizontal"
+                  invert={false}
+                  min={COMFORT_RANGE[0]}
+                  max={COMFORT_RANGE[1]}
+                  size="small"
+                  round="small"
+                  values={comfortRange}
+                  onChange={setComfortRange}
+              />
+            </Box>
+            <Box align="center" justify="between" gap="medium" pad="small" basis="1/4"
+                 style={{marginTop: "14px"}}>
+              <MultiToggle
+                  options={USER_REVIEW_FILTER}
+                  selectedOption={userReviewStatus}
+                  onSelectOption={setUserReviewStatus}
+                  label="Show offers"
+              />
+            </Box>
+          </Box>
+
+          <Box align="center" justify="center" direction="column" pad="medium">
+            <DropButton label="Sortowanie" dropAlign={{"top": "bottom"}} dropContent={(
+                <Box align="center" justify="center" pad="medium" name="dropContent" background={"black"}
+                     animation="fadeIn" hoverIndicator={false} border={{"size": "small"}} gap="small">
+                  <SortItem icon={Achievement} sortObj={{rank: "desc"}} {...sortByItemProps}>
+                    Ocena
+                  </SortItem>
+                  <SortItem icon={History} sortObj={{createdAt: "desc"}} {...sortByItemProps}>
+                    Od najnowszych
+                  </SortItem>
+                  <SortItem icon={Lounge} sortObj={{indicators_comfort: "desc"}} {...sortByItemProps}>
+                    Komfort
+                  </SortItem>
+                  <SortItem icon={Lounge} sortObj={{deviationAvgM2Price: "asc"}} {...sortByItemProps}>
+                    Odchyl. cenowe 📈
+                  </SortItem>
+                  <SortItem icon={Currency} sortObj={{indicators_deal: "desc"}} {...sortByItemProps}>
+                    Okazja cenowa
+                  </SortItem>
+                  <SortItem sortObj={{prices_perM2: "asc"}} {...sortByItemProps}>
+                    Cena m²
+                  </SortItem>
+                  <SortItem icon={Ascend} sortObj={{prices_full: "asc"}} {...sortByItemProps}>
+                    Cena {/* rosnaco */}
+                  </SortItem>
+                  <SortItem icon={Descend} sortObj={{prices_full: "desc"}} {...sortByItemProps}>
+                    Cena {/* malejaco */}
+                  </SortItem>
+                  <SortItem icon={Ascend} sortObj={{attrs_area: "asc"}} {...sortByItemProps}>
+                    Powierzchnia {/* rosnaco */}
+                  </SortItem>
+                  <SortItem icon={Descend} sortObj={{attrs_area: "desc"}} {...sortByItemProps}>
+                    Powierzchnia {/* malejaco */}
+                  </SortItem>
+
+                </Box>
+            )}
+                        plain={false} reverse={false} dropProps={{"elevation": "xsmall", "plain": true}}/>
+          </Box>
         </Box>
       </Box>
   )
