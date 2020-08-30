9651a9304da15e04b11df6af314d32dcbd74036f
Dariusz Kobuszewski <quack.code.dk@gmail.com>
2020-08-30 15:29:20 +0200

    client: card indicators wrapping

--- client/components/Offer/OfferCard/OfferCard.js
+++ client/components/Offer/OfferCard/OfferCard.js
@@ -33,80 +33,79 @@ if (typeof atob === 'undefined') {
 const _OfferCard = React.forwardRef(({measure, style, ...offer}, cellMeasureRef) => {
 
 
-  const {
-    id,
-    title, url, district, description, street,
+      const {
+        id,
+        title, url, district, description, street,
 
-    userReviewStatus
-  } = offer;
-  const {
-    favoriteColor, rejectedColor,
-    toggleLike, toggleReject, updateStreet,
-    locationIcon,
-    indicators_comfort, indicators_deal, descriptionRating, rank
-  } = offer.logic;
-  const delayMeasure = () => setTimeout(measure, 100);
+        userReviewStatus
+      } = offer;
+      const {
+        favoriteColor, rejectedColor,
+        toggleLike, toggleReject, updateStreet,
+        locationIcon,
+        indicators_comfort, indicators_deal, descriptionRating, rank
+      } = offer.logic;
+      const delayMeasure = () => setTimeout(measure, 100);
 
 
-  return <Box align="center" justify="center" pad="small" background={{"color": "light-1", "opacity": "weak"}}
-              round="medium" ref={cellMeasureRef} className="offer-card" style={style}>
+      return <Box align="center" justify="center" pad="small" background={{"color": "light-1", "opacity": "weak"}}
+                  round="medium" ref={cellMeasureRef} className="offer-card" style={style}>
 
-    <CardTopGallery {...offer} onImageLoad={measure}/>
+        <CardTopGallery {...offer} onImageLoad={measure}/>
 
-    <Box align="stretch" justify="start" direction="column" fill={true} flex="shrink"
-         pad="small">
-      <Box align="center" justify="between" direction="row" gap="medium" fill="horizontal">
-        <Heading textAlign="start" truncate={false} size="medium" level="3">
-          <Anchor href={url} target="_blank" color="white">
-            {title}
-          </Anchor>
-        </Heading>
-        <Box align="center" justify="center" direction="row" gap="small" pad="small" className="actions">
-          <Button icon={<Favorite color={favoriteColor}/>} type="button" onClick={toggleLike} />
-          <Button icon={<Clear color={rejectedColor}/>} type="button" onClick={toggleReject} />
-        </Box>
-      </Box>
-      <div className="middle-infos">
-        <div className="location">
-          {locationIcon}&nbsp;
-          {district},&nbsp;<InlineInput value={street} placeholder="(ulica)"
-                                        fontSize={13} onSubmit={updateStreet} />
-        </div>
-        <div className="indicators">
-          <Box align="center" justify="center" direction="row" gap="small" pad={{"left": "medium"}}>
-            <Box align="center" justify="center" direction="row" gap="xsmall" style={{wordWrap:"nowrap"}}>
-              <Text size="small">☆ {rank}</Text>
-            </Box>
-            <Box align="center" justify="center" direction="row" gap="xsmall">
-              <Lounge size="small"/>
-              <Text size="small">{indicators_comfort}</Text>
-            </Box>
-            <Box align="center" justify="center" direction="row" gap="xsmall">
-              <Task size="small"/>
-              <Text size="small">{descriptionRating}</Text>
-            </Box>
-            <Box align="center" justify="center" direction="row" gap="xsmall">
-              <Currency size="small"/>
-              <Text size="small">{indicators_deal}</Text>
+        <Box align="stretch" justify="start" direction="column" fill={true} flex="shrink"
+             pad="small">
+          <Box align="center" justify="between" direction="row" gap="medium" fill="horizontal">
+            <Heading textAlign="start" truncate={false} size="medium" level="3">
+              <Anchor href={url} target="_blank" color="white">
+                {title}
+              </Anchor>
+            </Heading>
+            <Box align="center" justify="center" direction="row" gap="small" pad="small" className="actions">
+              <Button icon={<Favorite color={favoriteColor}/>} type="button" onClick={toggleLike}/>
+              <Button icon={<Clear color={rejectedColor}/>} type="button" onClick={toggleReject}/>
             </Box>
           </Box>
-        </div>
-      </div>
-      <Box align="stretch" justify="center" gap="xxsmall" flex="grow">
-        <Accordion animate={false} fill="vertical" className="description" onActive={delayMeasure}>
-          <AccordionPanel label="Opis" panelBorder={0}>
-            <Box align="stretch" justify="start" flex={false} direction="row" basis="small" pad="small"
-                 alignContent="stretch" flex="grow" overflow={"auto"}>
-              <Paragraph fill="vertical" size="small" flex={{grow: 1.5}} width={"large"} basis="medium">
-                {description}
-              </Paragraph>
+          <div className="middle-infos">
+            <div className="location">
+              {locationIcon}&nbsp;
+              {district},&nbsp;<InlineInput value={street} placeholder="(ulica)"
+                                            fontSize={13} onSubmit={updateStreet}/>
+            </div>
+            <Box className="indicators" align="center" justify="center" direction="row" gap="small"
+                 pad={{"left": "medium"}}>
+              <Box align="center" justify="center" direction="row" style={{wordWrap: "nowrap"}}>
+                <Text size="small">☆ {rank}</Text>
+              </Box>
+              <Box align="center" justify="center" direction="row">
+                <Lounge size="small"/>
+                <Text size="small">&nbsp;{indicators_comfort}</Text>
+              </Box>
+              <Box align="center" justify="center" direction="row">
+                <Task size="small"/>
+                <Text size="small">&nbsp;{descriptionRating}</Text>
+              </Box>
+              <Box align="center" justify="center" direction="row">
+                <Currency size="small"/>
+                <Text size="small">&nbsp;{indicators_deal}</Text>
+              </Box>
             </Box>
-          </AccordionPanel>
-        </Accordion>
-      </Box>
-    </Box>
-  </Box>;
-}
+          </div>
+          <Box align="stretch" justify="center" gap="xxsmall" flex="grow">
+            <Accordion animate={false} fill="vertical" className="description" onActive={delayMeasure}>
+              <AccordionPanel label="Opis" panelBorder={0}>
+                <Box align="stretch" justify="start" flex={false} direction="row" basis="small" pad="small"
+                     alignContent="stretch" flex="grow" overflow={"auto"}>
+                  <Paragraph fill="vertical" size="small" flex={{grow: 1.5}} width={"large"} basis="medium">
+                    {description}
+                  </Paragraph>
+                </Box>
+              </AccordionPanel>
+            </Accordion>
+          </Box>
+        </Box>
+      </Box>;
+    }
 )
 const OfferCard = withOfferLogic(_OfferCard, {skipGrommet: false})
 
